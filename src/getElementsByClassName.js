7d283dfe3800c4ea10587497dca8375ca4d24f95
makardjian <makardjian@gmail.com>
2018-11-05 13:34:34 -0800

    pseudocde entered before lunch

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,27 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
 };
+
+/*
+
+input: a target class name 
+output: returns an array like object
+
+pseudocode:
+  /declare a a variable called 'classes' to hold the nodes that contain the target className
+  
+  /declare a helper function called getClasses which takes in a node
+    /if the node has a classList and the target className exists as a class on the node, then push the entire node into 'classes' array.
+    
+    /else if the node has childNodes, then:
+        /call the inner function on each childNode using forEach. 
+    
+  /invoke the inner function getClasses(node)
+
+  /return classes array. 
+
+
+*/
